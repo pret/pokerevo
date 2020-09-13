6351f79a77797a69ec1a0ed8acc923128b09a514
red031000 <rubenru09@aol.com>
2020-09-13 17:01:11 +0100

    split OSAudioSystem

--- obj_files.mk
+++ obj_files.mk
@@ -19,6 +19,7 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/SDK/OS/OSAlarm.o               \
 	$(BUILD_DIR)/asm/SDK/OS/OSAlloc.o               \
 	$(BUILD_DIR)/asm/SDK/OS/OSArena.o               \
+	$(BUILD_DIR)/asm/SDK/OS/OSAudioSystem.o         \
 	$(BUILD_DIR)/asm/text_7_2.o                     \
 	$(BUILD_DIR)/asm/text_8.o                       \
 	$(BUILD_DIR)/asm/text_9.o                       \
