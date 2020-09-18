be2afe5320d75df020776a732fb2a800d0c775c2
red031000 <rubenru09@aol.com>
2020-09-18 16:24:51 +0100

    resolve some ctors

--- asm/ctors.s
+++ asm/ctors.s
@@ -6,4 +6,6 @@
 lbl_803E1E60:
 	.4byte __init_cpp_exceptions_tmp, func_8000746C, func_80007920, func_800097B8
 	.4byte func_8000A7A4, func_8000AAA4, func_8000AD5C, func_80024900
-	.incbin "baserom.dol", 0x3DDF80, 0x420
+	.4byte func_80025370, func_80028490, func_8002A8F0, func_8002ABC8
+	.4byte func_8002BB24, func_8002CAE0, func_8002DE0C, func_80030114
+	.incbin "baserom.dol", 0x3DDFA0, 0x400
