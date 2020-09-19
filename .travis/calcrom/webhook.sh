be7e10db84d0df366a8adabd2ed8931702be195f
red031000 <rubenru09@aol.com>
2020-09-19 22:00:35 +0100

    test

--- .travis/calcrom/webhook.sh
+++ .travis/calcrom/webhook.sh
@@ -11,7 +11,7 @@ if [ ! -f $map_file ]; then
   echo "$map_file does not exist!"
   exit 1
 fi
-
+echo "1"
 make -C ${TRAVIS_BUILD_DIR}/.travis/calcrom
 output=$(${TRAVIS_BUILD_DIR}/.travis/calcrom/calcrom ${TRAVIS_BUILD_DIR} | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
 curl -d "{\"username\": \"$CALCROM_DISCORD_WEBHOOK_USERNAME\", \"avatar_url\": \"$CALCROM_DISCORD_WEBHOOK_AVATAR_URL\", \"content\":\"\`\`\`$build_name progress:\\n$output\`\`\`\"}" -H "Content-Type: application/json" -X POST $CALCROM_DISCORD_WEBHOOK_URL
