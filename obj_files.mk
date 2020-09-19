df38c19dda4c78de1d1109fd19560c2e0f4872a5
Max <mparisi@stevens.edu>
2020-09-19 18:07:36 -0400

    split gtMessage.o

--- obj_files.mk
+++ obj_files.mk
@@ -67,6 +67,7 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/gt2Callback.o                                \
 	$(BUILD_DIR)/asm/gt2Connection.o                              \
 	$(BUILD_DIR)/asm/gt2Main.o                                    \
+	$(BUILD_DIR)/asm/gt2Message.o                                 \
 	$(BUILD_DIR)/asm/text_10_2.o                                  \
 	$(BUILD_DIR)/asm/text_11.o
 
