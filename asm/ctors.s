59a42638179205f497923fdf09c9ead723bd0f42
ProjectRevoTPP <projectrevotpp@hotmail.com>
2020-09-25 19:31:35 -0400

    dump and label ctors table

--- asm/ctors.s
+++ asm/ctors.s
@@ -9,4 +9,68 @@ lbl_803E1E60:
 	.4byte func_80025370, func_80028490, func_8002A8F0, func_8002ABC8
 	.4byte func_8002BB24, func_8002CAE0, func_8002DE0C, func_80030114
 	.4byte func_80030300, func_80030B80, func_800310B8, func_80031524
-	.incbin "baserom.dol", 0x3DDFB0, 0x3F0
+    .4byte func_800321C4, func_80032488, func_80032BA8, func_80032E40
+    .4byte func_80032FC8, func_80035C90, func_80036F68, func_8003E01C
+    .4byte func_8003E834, func_800400C8, func_800447C4, func_80044F08
+    .4byte func_800477C8, func_800478A0, func_80048708, func_80049064
+    .4byte func_8004ACD4, func_8004D704, func_8004DE68, func_8004E9D0
+    .4byte func_8004F35C, func_8005016C, func_80050304, func_8005547C
+    .4byte func_800557E4, func_800598D8, func_8005A524, func_8005B484
+    .4byte func_8005CE74, func_8006A864, func_8006E534, func_8007CF90
+    .4byte func_8007F878, func_8008A298, func_8008D8AC, func_80091248
+    .4byte func_80094DA0, func_8009596C, func_8009A29C, func_8009C188
+    .4byte func_8009F6A4, func_800AA018, func_800AA450, func_800B9DFC
+    .4byte func_800C5948, func_800D7C7C, func_800E4220, func_800E9668
+    .4byte func_800F8A8C, func_80104334, func_801166BC, func_8011B17C
+    .4byte func_8011C934, func_8011C954, func_8011E1DC, func_8012F0C4
+    .4byte func_80131CD0, func_8013A910, func_8013F030, func_8014FE88
+    .4byte func_80151454, func_80155B30, func_80157834, func_80158130
+    .4byte func_80158E30, func_8015D358, func_80162764, func_80162B50
+    .4byte func_801633EC, func_80163654, func_80164CAC, func_80164E1C
+    .4byte func_80164F80, func_80165514, func_80166D4C, func_80167278
+    .4byte func_80167800, func_80167974, func_801679D8, func_801682B0
+    .4byte func_80168778, func_80168BFC, func_80169248, func_8016978C
+    .4byte func_8016A08C, func_8016E298, func_8016EFAC, func_801708BC
+    .4byte func_80176E94, func_80177C04, func_8017B498, func_8017C568
+    .4byte func_8017C960, func_8017CC78, func_8017E3E0, func_8017E940
+    .4byte func_80185ACC, func_80187768, func_8019BEA4, func_801DAAC0
+    .4byte func_801DB020, func_801DB7FC, func_801DBB1C, func_801DBE70
+    .4byte func_801DCE4C, func_801DD5A8, func_801DD8A0, func_801DDF58
+    .4byte func_801DF020, func_801E07C8, func_801E07F0, func_801E0A34
+    .4byte func_801E4308, func_801E472C, func_801E5F5C, func_801E6BD8
+    .4byte func_801ED364, func_801EE024, func_801F1A68, func_801F40C0
+    .4byte func_801F98CC, func_801FA074, func_801FA36C, func_801FB40C
+    .4byte func_801FF2E8, func_801FFF94, func_80202E34, func_80203FE8
+    .4byte func_80205934, func_80207898, func_80207C2C, func_8020B4D8
+    .4byte func_8020CA18, func_8020D230, func_8020EA58, func_8020F8D4
+    .4byte func_80211544, func_802119E4, func_80213674, func_80219E28
+    .4byte func_8021C200, func_8021CA80, func_8021D0C0, func_8021D404
+    .4byte func_8021EA28, func_8021FB24, func_8022061C, func_80220D88
+    .4byte func_802233A8, func_8022362C, func_802236A0, func_802237F0
+    .4byte func_80223870, func_802239EC, func_80223B20, func_80223C34
+    .4byte func_802240E4, func_80224780, func_8022492C, func_80224AF0
+    .4byte func_80225E24, func_80226344, func_8022771C, func_80228950
+    .4byte func_80228AEC, func_802290EC, func_8022D70C, func_80230BB4
+    .4byte func_80230BD4, func_802322EC, func_8023281C, func_80232D04
+    .4byte func_802353D8, func_80235454, func_80235A54, func_802366A0
+    .4byte func_8023692C, func_80236AE8, func_80237774, func_8023779C
+    .4byte func_8023AF6C, func_8023B62C, func_8023B9D4, func_8023CF8C
+    .4byte func_8023EA24, func_8023EE4C, func_80240040, func_8024050C
+    .4byte func_802437CC, func_802448C8, func_802449F8, func_80244EBC
+    .4byte func_802452C8, func_80245C40, func_80246CAC, func_80247018
+    .4byte func_80247260, func_80249B5C, func_80249B80, func_80249BA8
+    .4byte func_80249BD0, func_80249D18, func_80250EE0, func_80253FB4
+    .4byte func_80254240, func_80254440, func_8025714C, func_80259A84
+    .4byte func_8025A6F0, func_8025AD64, func_8025B144, func_8025B68C
+    .4byte func_8025B6C8, func_8025D398, func_8025D7AC, func_8025DCA0
+    .4byte func_8025DF60, func_8025E328, func_8025E84C, func_8025EC48
+    .4byte func_8025F05C, func_8025FFD8, func_802609C0, func_8026136C
+    .4byte func_80262AE0, func_80262E70, func_80263C9C, func_802674AC
+    .4byte func_80268208, __sinit_$3ut_IOStream_cpp, __sinit_$3ut_FileStream_cpp, __sinit_$3ut_DvdFileStream_cpp
+    .4byte ut_DvdLockedFileStream_cpp, __sinit_$3ut_TextWriterBase_cpp, __sinit_$3snd_BasicSound_cpp, func_80375360
+    .4byte func_8037C408, func_8037F8FC, func_80383998, func_80385E14
+    .4byte func_803872D0, func_80387EA8, func_8038A320, func_8038A3CC
+    .4byte func_8038F254, func_803D91F4, func_803D9EA0
+
+    # placed by the compiler it seems
+    .balign 0x20, 0
