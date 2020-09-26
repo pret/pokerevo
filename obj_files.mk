1fc8b9181eaa28852f9ae17d6e36d6d5662ed79e
Max <mparisi@stevens.edu>
2020-09-26 09:19:18 -0400

    split snd_SoundHeap.s

--- obj_files.mk
+++ obj_files.mk
@@ -144,6 +144,7 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/nw4r/snd/detail/snd_SoundArchiveLoader.o         \
 	$(BUILD_DIR)/asm/nw4r/snd/snd_SoundArchivePlayer.o                \
 	$(BUILD_DIR)/asm/nw4r/snd/snd_SoundHandle.o                       \
+	$(BUILD_DIR)/asm/nw4r/snd/snd_SoundHeap.o                         \
 	$(BUILD_DIR)/asm/text_10_6.o                                      \
 	$(BUILD_DIR)/asm/text_11.o
 
