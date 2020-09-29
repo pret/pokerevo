34fdfac1e8576d404612b2cafb9344160b4a20af
Max <mparisi@stevens.edu>
2020-09-29 10:45:40 -0400

    split ansi_files.s

--- asm/text_6.s
+++ asm/text_6.s
@@ -4,8 +4,8 @@
 
 .global func_801CA7B0
 func_801CA7B0:
-/* 801CA7B0 001C6410  3C 60 80 1C */	lis r3, func_801C7894@ha
-/* 801CA7B4 001C6414  38 63 78 94 */	addi r3, r3, func_801C7894@l
+/* 801CA7B0 001C6410  3C 60 80 1C */	lis r3, __close_all@ha
+/* 801CA7B4 001C6414  38 63 78 94 */	addi r3, r3, __close_all@l
 /* 801CA7B8 001C6418  90 6D 9F D8 */	stw r3, lbl_8063F298-_SDA_BASE_(r13)
 /* 801CA7BC 001C641C  4E 80 00 20 */	blr
 
