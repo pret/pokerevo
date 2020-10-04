0429c3fe790e2c4ab550ec016a96a382e194c6bc
Max <mparisi@stevens.edu>
2020-10-04 15:36:27 -0400

    split KPAD.s

--- obj_files.mk
+++ obj_files.mk
@@ -211,6 +211,7 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/SDK/TPL/TPL.o                                     \
 	$(BUILD_DIR)/asm/SDK/THP/THPDec.o                                     \
 	$(BUILD_DIR)/asm/SDK/THP/THPAudio.o                                     \
+	$(BUILD_DIR)/asm/SDK/KPAD/KPAD.o                                     \
 	$(BUILD_DIR)/asm/text_8_6.o                                     \
 	$(BUILD_DIR)/asm/text_9.o                                     \
 	$(BUILD_DIR)/asm/text_10.o                                    \
