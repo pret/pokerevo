937a9a8b16bcd521e78f733d7c630f9f68a04a04
Max <mparisi@stevens.edu>
2020-10-04 16:53:02 -0400

    split ipcMain.s and ipcclt.s

--- obj_files.mk
+++ obj_files.mk
@@ -213,6 +213,10 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/SDK/THP/THPAudio.o                                     \
 	$(BUILD_DIR)/asm/SDK/KPAD/KPAD.o                                     \
 	$(BUILD_DIR)/asm/text_8_6.o                                     \
+	$(BUILD_DIR)/asm/SDK/ipcMain.o                                     \
+	$(BUILD_DIR)/asm/SDK/ipcclt.o                                     \
+	$(BUILD_DIR)/asm/text_8_7.o                                     \
+	$(BUILD_DIR)/asm/text_8_8.o                                     \
 	$(BUILD_DIR)/asm/text_9.o                                     \
 	$(BUILD_DIR)/asm/text_10.o                                    \
 	$(BUILD_DIR)/src/GameSpy/darray.o                             \
