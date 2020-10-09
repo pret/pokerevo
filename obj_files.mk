191ebc6d42270ffb1c6313de067433116199358a
Max <mparisi@stevens.edu>
2020-10-09 14:07:40 -0400

    split NHTTP_bgnend.s

--- obj_files.mk
+++ obj_files.mk
@@ -335,6 +335,7 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/SDK/SO/SOBasic.o                                     \
 	$(BUILD_DIR)/asm/SDK/SO/SOInformation.o                                     \
 	$(BUILD_DIR)/asm/SDK/SO/SOOption.o                                     \
+	$(BUILD_DIR)/asm/SDK/NHTTP/NHTTP_bgnend.o                                     \
 	$(BUILD_DIR)/asm/text_9.o                                     \
 	$(BUILD_DIR)/asm/text_10.o                                    \
 	$(BUILD_DIR)/src/GameSpy/darray.o                             \
