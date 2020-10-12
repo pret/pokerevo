a3484499760ebd60bfab145b80651cafba28b014
Max <mparisi@stevens.edu>
2020-10-11 21:51:07 -0400

    split NWC24Config, NWC24FileAPI, NWC24MBoxCtrl, NWC24Manage

--- obj_files.mk
+++ obj_files.mk
@@ -352,6 +352,10 @@ TEXT_O_FILES := 						                          \
 	$(BUILD_DIR)/asm/SDK/SSL/ssl_mutex.o                                     \
 	$(BUILD_DIR)/asm/text_9.o                                     \
 	$(BUILD_DIR)/asm/SDK/NWC24/NWC24StdAPI.o                                     \
+	$(BUILD_DIR)/asm/SDK/NWC24/NWC24FileAPI.o                                     \
+	$(BUILD_DIR)/asm/SDK/NWC24/NWC24Config.o                                     \
+	$(BUILD_DIR)/asm/SDK/NWC24/NWC24Manage.o                                     \
+	$(BUILD_DIR)/asm/SDK/NWC24/NWC24MBoxCtrl.o                                     \
 	$(BUILD_DIR)/asm/text_9_2.o                                     \
 	$(BUILD_DIR)/asm/SDK/VF/pf_clib.o                                     \
 	$(BUILD_DIR)/asm/SDK/VF/pf_code.o                                     \
