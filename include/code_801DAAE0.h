f4bba77e25e70dc342724435af05d3ae1fe19c5b
Max <mparisi@stevens.edu>
2020-10-18 00:06:45 -0400

    match func_801DAB28, func_801DAB30, and func_801DAB78

--- include/code_801DAAE0.h
+++ include/code_801DAAE0.h
@@ -5,15 +5,22 @@
 extern "C" {
 #endif
 
+// TODO: move
+typedef u32 MEMHeapHandle;
+
+
 class unkClass
 {
 public:
     u8 unk0;
-    u8 unk1[11];
+    MEMHeapHandle unk4;
+    u32 unk8;
 };
 
 unkClass* func_801DAAE0(void);
 BOOL func_801DAB28(void);
+void func_801DAB30(void);
+MEMHeapHandle func_801DAB78(void* p1, u32 p2, u16 p3);
 void* func_801DAD64(size_t);
 void func_801DAEA4(void*);
 
--- src/code_801DAAE0.cpp
+++ src/code_801DAAE0.cpp
@@ -7,6 +7,14 @@
 extern "C" {
 
 // unkStruct gUnk8063F2D0(1, 4, 0);
+// TODO: move
+extern u8 lbl_8063F2D6;
+extern u8 lbl_8063F2D7;
+extern u32 lbl_8063F2D8;
+extern u16 lbl_8063F2DC;
+void *memset(void *s, int c, size_t n);
+MEMHeapHandle MEMCreateExpHeapEx(void* p1, u32 p2, u16 p3);
+
 
 unkClass gUnk804912B0[16];
 
@@ -16,6 +24,34 @@ unkClass* func_801DAAE0(void)
         if (gUnk804912B0[i].unk0 == 0)
             return &gUnk804912B0[i];
     return NULL;
+}
+
+BOOL func_801DAB28(void)
+{
+    return lbl_8063F2D6;
+}
+
+void func_801DAB30(void)
+{
+    memset(gUnk804912B0, 0, sizeof(gUnk804912B0));
+    lbl_8063F2D8 = 0;
+    lbl_8063F2DC = 0;
+    lbl_8063F2D6 = 1;
+    lbl_8063F2D7 = 0;
+}
+
+MEMHeapHandle func_801DAB78(void* p1, u32 p2, u16 p3)
+{
+    if (!func_801DAB28())
+        return 0;
+    unkClass* r31 = func_801DAAE0();
+    if (!r31)
+        return 0;
+    MEMHeapHandle r3 = MEMCreateExpHeapEx(p1, p2, p3);
+    r31->unk0 = 1;
+    r31->unk4 = r3;
+    r31->unk8 = p2;
+    return r3;
 }
 
 }
