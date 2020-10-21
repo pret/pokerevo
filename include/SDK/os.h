2b2dc15d6b79b51ffb09a9d1698bfa687363c242
Max <mparisi@stevens.edu>
2020-10-21 00:07:51 -0400

    match func_801DB92C

---
+++ include/SDK/os.h
@@ -0,0 +1,15 @@
+#ifndef POKEREVO_OS_H
+#define POKEREVO_OS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "SDK/OS/OSAlloc.h"
+#include "SDK/OS/OSCache.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //POKEREVO_OS_H
